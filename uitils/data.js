4a416e19810351bd62d0b91f22d06516fa89190e
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-10-19 12:16:39 +0200

    Create temporary data file

---
+++ uitils/data.js
@@ -0,0 +1,114 @@
+export const brandsPhoneData = [
+  {
+    name: "Apple",
+    category: "Phone",
+    image: "/images/brand1.jpg",
+    imgPhones: "/images/phones1.jpg",
+    url: "/apple",
+    icon: "/images/icon2.png",
+  },
+  {
+    name: "Huawei",
+    category: "Phone",
+    image: "/images/brand2.jpg",
+    imgPhones: "/images/phones2.jpg",
+    url: "/huawei",
+    icon: "/images/icon4.png",
+  },
+  {
+    name: "Lenovo",
+    category: "Phone",
+    image: "/images/brand3.jpg",
+    imgPhones: "/images/phones3.jpg",
+    url: "/lenovo",
+  },
+  {
+    name: "Nokia",
+    category: "Phone",
+    image: "/images/brand4.jpg",
+    imgPhones: "/images/phones4.jpg",
+    url: "/nokia",
+  },
+  {
+    name: "Samsung",
+    category: "Phone",
+    image: "/images/brand5.jpg",
+    imgPhones: "/images/phones5.jpg",
+    url: "/samsung",
+  },
+  {
+    name: "Xiaomi",
+    category: "Phone",
+    image: "/images/brand6.jpg",
+    imgPhones: "/images/phones6.jpg",
+    url: "/xiaomi",
+    icon: "/images/icon1.png",
+  },
+  {
+    name: "OnePlus",
+    category: "Phone",
+    image: "/images/brand7.jpg",
+    imgPhones: "/images/phones7.jpg",
+    url: "/oneplus",
+    icon: "/images/icon3.png",
+  },
+  {
+    name: "Vivo",
+    category: "Phone",
+    image: "/images/brand8.jpg",
+    imgPhones: "/images/phones8.jpg",
+    url: "/vivo",
+  },
+];
+
+export const sliderData = [
+  {
+    id: 1,
+    image: "/images/vivo-y91.png",
+    alt: "Vivo y91",
+    title: "Vivo Y91",
+    description:
+      "Vivo Y91 will let you multitask on the go, without worrying about ...",
+    redirect: "/vivo",
+  },
+  {
+    id: 2,
+    image: "/images/redmi.png",
+    alt: "Desert",
+    title: "Xiaomi Redmi Note 10 Pro",
+    description:
+      "Modern, elegant and efficient. This smartphone model is one of the best choices ...",
+    redirect: "/xiaomi",
+  },
+  {
+    id: 3,
+    image: "/images/oneplus-8-1.png",
+    alt: "Oneplus 8",
+    title: "Oneplus 8",
+    description:
+      "The OnePlus 8 has a 6.55-inch AMOLED panel with sloping sides, a wide colour gamut ...",
+    redirect: "/oneplus",
+  },
+];
+
+export const samsungPhones = [
+  {
+    name: "Samsung",
+    category: "",
+    image: "/images/phone1.jpg",
+    price: 85,
+    rate: 5,
+    onStock: 23,
+    description: "",
+    reviews: [
+      { user: "Joe Doe", avatar: "/images/user1.jpg", review: "", rate: 5 },
+      {
+        user: "Mike Berry",
+        avatar: "/images/user2.jpg",
+        review: "",
+        rate: 4.5,
+      },
+      { user: "Alice Key", avatar: "/images/user2.jpg", review: "", rate: 4 },
+    ],
+  },
+];
