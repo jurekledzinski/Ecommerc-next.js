d9f421b4d31c4643a60e70624dfbe6c7ac0ea85a
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-10-21 17:47:57 +0200

    Create hamburger menu

---
+++ components/HamburgerMenu.js
@@ -0,0 +1,13 @@
+import { MenuIcon, MenuLine } from "../muistyles/Navbar.styles";
+
+const HamburgerMenu = () => {
+  return (
+    <MenuIcon>
+      <MenuLine />
+      <MenuLine />
+      <MenuLine />
+    </MenuIcon>
+  );
+};
+
+export default HamburgerMenu;
