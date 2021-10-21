efde9367cc14b7613fd2eca53863ebaf7b80c00b
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-10-21 17:49:26 +0200

    Create menu component

---
+++ components/Menu.js
@@ -0,0 +1,69 @@
+import React, { useContext, useRef } from "react";
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import {
+  listStyles,
+  listItemStyles,
+  menuTitleStyles,
+  Nav,
+} from "../muistyles/Navbar.styles";
+
+import { StoreContext } from "../uitils/store";
+
+import {
+  CLOSE_DRAWER,
+  SHOW_MENU,
+  OPEN_DRAWER,
+  SHOW_SIGN_IN,
+  SHOW_SIGN_UP,
+} from "../uitils/constants";
+
+const Menu = () => {
+  const { disptachContentDrawer, disptachOpenDrawer } =
+    useContext(StoreContext);
+  const idTimeout = useRef(null);
+  const idTimeoutSec = useRef(null);
+
+  const handleItemMenu = (e) => {
+    console.log(e.target.textContent);
+
+    switch (e.target.textContent) {
+      case "Sign In":
+        disptachOpenDrawer({ type: CLOSE_DRAWER });
+
+        idTimeout.current = setTimeout(() => {
+          disptachOpenDrawer({ type: OPEN_DRAWER });
+          disptachContentDrawer({ type: SHOW_SIGN_IN });
+          clearTimeout(idTimeout.current);
+        }, 800);
+        break;
+      case "Sign Up":
+        disptachOpenDrawer({ type: CLOSE_DRAWER });
+
+        idTimeout.current = setTimeout(() => {
+          disptachOpenDrawer({ type: OPEN_DRAWER });
+          disptachContentDrawer({ type: SHOW_SIGN_UP });
+          clearTimeout(idTimeoutSec.current);
+        }, 800);
+        break;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <Nav>
+      <Typography variant="h6" sx={menuTitleStyles}>
+        Welcome
+      </Typography>
+      <List sx={listStyles}>
+        {["Home", "Contact", "Sign In", "Sign Up"].map((item) => (
+          <ListItem button key={item} onClick={handleItemMenu}>
+            <ListItemText primary={item} sx={listItemStyles} />
+          </ListItem>
+        ))}
+      </List>
+    </Nav>
+  );
+};
+
+export default Menu;
