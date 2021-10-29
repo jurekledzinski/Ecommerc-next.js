33bb0e165ff8ddd4e93e4a9c7b4d56d270d556be
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-10-29 22:45:06 +0200

    Add layout styles component

--- muistyles/Layout.styles.js
+++ muistyles/Layout.styles.js
@@ -1,38 +1,42 @@
-import { styled } from "@mui/material/styles";
+import { styled } from '@mui/material/styles';
 
 export const layoutContainerStyles = {
-  maxWidth: "1200px",
-  minHeight: { xs: "0vh", sm: "calc(100vh - 546px)" },
+  maxWidth: '1200px',
+  minHeight: { xs: '0vh', sm: 'calc(100vh - 546px)' },
+  backgroundColor: (theme) => theme.palette.background.paper,
 };
 
-export const Image = styled("img")(({ theme }) => ({
-  minWidth: "100%",
+export const Image = styled('img')(({ theme }) => ({
+  minWidth: '100%',
 }));
 
 export const wrapperTitleStyles = {
-  margin: "20px auto 20px auto",
-  maxWidth: "1200px",
-  paddingLeft: "24px",
+  maxWidth: '1200px',
+  padding: '20px 0 20px 24px',
+  margin: '0 auto',
+  backgroundColor: (theme) => theme.palette.background.paper,
 };
 
 export const titleProductsStyles = {
-  textTransform: "uppercase",
+  color: (theme) => theme.palette.text.primary,
+  textTransform: 'uppercase',
   fontWeight: 500,
-  fontFamily: "Oswald, sans-serif",
+  fontFamily: 'Oswald, sans-serif',
   letterSpacing: 1,
 };
 
 export const wrapperButtonsStyles = {
-  margin: "0 auto",
-  maxWidth: { xs: "100%", sm: "1200px" },
-  paddingLeft: { xs: "20px", sm: "24px" },
+  margin: '0 auto',
+  maxWidth: { xs: '100%', sm: '1200px' },
+  paddingLeft: { xs: '20px', sm: '24px' },
+  backgroundColor: (theme) => theme.palette.background.paper,
 };
 
 export const buttonGroupStyles = {
-  minWidth: { xs: "initial", sm: "initial" },
-  width: { xs: "calc(100% - 16px)", sm: "initial" },
+  minWidth: { xs: 'initial', sm: 'initial' },
+  width: { xs: 'calc(100% - 16px)', sm: 'initial' },
 };
 
 export const buttonProductStyles = {
-  width: { xs: "33.3%", sm: "initial" },
+  width: { xs: '33.3%', sm: 'initial' },
 };
