02b92c823a901dd3ab9b9f851cfcb811536e7c01
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-10-29 22:50:04 +0200

    Add darkmode reducer

--- uitils/store.js
+++ uitils/store.js
@@ -1,11 +1,20 @@
-import { createContext, useReducer } from "react";
-
+import { createContext, useReducer } from 'react';
+import Cookies from 'js-cookie';
 export const StoreContext = createContext();
 
-import { openDrawerReducer, contentDrawerReducer } from "./reducers";
+import { SHOW_MENU } from './constants';
+import {
+  openDrawerReducer,
+  contentDrawerReducer,
+  darkModeReducer,
+} from './reducers';
 
+const stateMode = Cookies.get('darkmode') === 'on' ? true : false;
 const initialStateOpenDrawer = { openDrawer: false };
-const initialStateContentDrawer = { contentDrawer: "SHOW_MENU" };
+const initialStateContentDrawer = { contentDrawer: SHOW_MENU };
+const initialStateDarkMode = {
+  darkmode: stateMode,
+};
 
 const StoreProvider = ({ children }) => {
   const [stateOpenDrawer, disptachOpenDrawer] = useReducer(
@@ -18,9 +27,16 @@ const StoreProvider = ({ children }) => {
     initialStateContentDrawer
   );
 
+  const [stateDarkMode, dispatchDarkMode] = useReducer(
+    darkModeReducer,
+    initialStateDarkMode
+  );
+
   return (
     <StoreContext.Provider
       value={{
+        stateDarkMode,
+        dispatchDarkMode,
         stateOpenDrawer,
         disptachOpenDrawer,
         stateContentDrawer,
