4708ed1dfd3dfb164ca21edb9700e8066dae6218
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-10-29 22:44:25 +0200

    Add dark mode

---
+++ components/ThemeMui.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { getTheme } from '../muistyles/ThemeMui';
+
+import { StoreContext } from '../uitils/store';
+
+const ThemeMui = ({ children }) => {
+  const { stateDarkMode } = useContext(StoreContext);
+  const { darkmode } = stateDarkMode;
+  const mode = darkmode ? 'dark' : 'light';
+  const darkModeTheme = createTheme(getTheme(mode));
+  return <ThemeProvider theme={darkModeTheme}>{children}</ThemeProvider>;
+};
+
+export default ThemeMui;
