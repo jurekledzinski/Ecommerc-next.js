1dc06a55be580c9b3de0828279916e2f90b82876
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-10-29 22:45:58 +0200

    Add styles to products

--- muistyles/Products.styles.js
+++ muistyles/Products.styles.js
@@ -1,28 +1,28 @@
-import { styled } from "@mui/material/styles";
+import { styled } from '@mui/material/styles';
 
-export const Section = styled("section")(({ theme }) => ({
-  maxWidth: "1200px",
-  margin: "0 auto",
-  [theme.breakpoints.up("xs")]: {
-    minHeight: "calc(100vh - 90px)",
+export const Section = styled('section')(({ theme }) => ({
+  maxWidth: '1200px',
+  margin: '0 auto',
+  [theme.breakpoints.up('xs')]: {
+    minHeight: 'calc(100vh - 90px)',
   },
-  [theme.breakpoints.up("sm")]: {
-    minHeight: "calc(100vh - 138px)",
+  [theme.breakpoints.up('sm')]: {
+    minHeight: 'calc(100vh - 139px)',
   },
 }));
 
-export const BreadLink = styled("a")(({ theme }) => ({
-  color: "#555555",
-  textDecoration: "none",
-  cursor: "pointer",
-  [theme.breakpoints.up("xs")]: {
-    fontSize: "1.4rem",
+export const BreadLink = styled('a')(({ theme }) => ({
+  color: theme.palette.text.primary,
+  textDecoration: 'none',
+  cursor: 'pointer',
+  [theme.breakpoints.up('xs')]: {
+    fontSize: '1.4rem',
   },
-  [theme.breakpoints.up("sm")]: {
-    fontSize: "1.6rem",
+  [theme.breakpoints.up('sm')]: {
+    fontSize: '1.6rem',
   },
-  transition: "0.2s ease",
-  ":hover": { color: "#1976d2" },
+  transition: '0.2s ease',
+  ':hover': { color: '#1976d2' },
 }));
 
 export const containerProductsStyles = {
@@ -30,47 +30,45 @@ export const containerProductsStyles = {
 };
 
 export const productTitleStyles = {
-  color: "black",
-  textTransform: "uppercase",
-  fontFamily: "Oswald, sans-serif",
+  color: (theme) => theme.palette.text.primary,
+  textTransform: 'uppercase',
+  fontFamily: 'Oswald, sans-serif',
   fontWeight: 500,
 };
 
 export const productBoxesStyles = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  height: "250px",
-  background: "#fbfbfb",
-  background:
-    "-webkit-linear-gradient(to right, #fbfbfb, #f8f8f8, #ffffff, #fbfbfb)",
-  background: "linear-gradient(to right, #fbfbfb, #f8f8f8, #ffffff, #fbfbfb)",
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '250px',
+  background: (theme) => theme.palette.background.cardtop,
 };
 
 export const cardStyles = {
-  height: "350px",
+  height: '350px',
 };
 
 export const aTagStyles = {
-  textDecoration: "none",
+  textDecoration: 'none',
 };
 
 export const cardMediaStyles = {
-  objectFit: "contain",
-  width: "200px",
-  height: "200px",
+  objectFit: 'contain',
+  width: '200px',
+  height: '200px',
 };
 
 export const cardContentStyles = {
-  height: "100px",
-  padding: "5px",
+  height: '100px',
+  padding: '5px',
+  background: (theme) => theme.palette.background.cardbottom,
 };
 
 export const boxWrapper = {
-  display: "flex",
-  width: "100%",
-  justifyContent: "space-between",
-  alignItems: "flex-end",
-  marginTop: "20px",
+  display: 'flex',
+  width: '100%',
+  justifyContent: 'space-between',
+  alignItems: 'flex-end',
+  marginTop: '20px',
 };
