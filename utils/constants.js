dad525916123c4df7b0c1122c83f82d40855511a
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-02 10:22:04 +0100

    Add constants file

---
+++ utils/constants.js
@@ -0,0 +1,8 @@
+export const OPEN_DRAWER = 'OPEN_DRAWER';
+export const CLOSE_DRAWER = 'CLOSE_DRAWER';
+export const SHOW_CART = 'SHOW_CART';
+export const SHOW_MENU = 'SHOW_MENU';
+export const SHOW_SIGN_IN = 'SHOW_SIGN_IN';
+export const SHOW_SIGN_UP = 'SHOW_SIGN_UP';
+export const SHOW_FORGET_PASSWORD = 'SHOW_FORGET_PASSWORD';
+export const TOGGLE_DARK_MODE = 'TOGGLE_DARK_MODE';
