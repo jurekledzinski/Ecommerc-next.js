b38132c701495da2af5f9479e85c0a5727bf19be
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-02 10:15:29 +0100

    Add custom breakpoints in theme in material ui

--- muistyles/ThemeMui.js
+++ muistyles/ThemeMui.js
@@ -29,6 +29,15 @@ export const getTheme = (mode) => ({
           },
         }),
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
 });
 
 // const theme = createTheme({
