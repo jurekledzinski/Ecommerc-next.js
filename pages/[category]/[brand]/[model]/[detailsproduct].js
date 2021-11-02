3ef7a029d9e5f6744197d408b14952aab7491bb7
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-02 10:17:11 +0100

    Create details product page

---
+++ pages/[category]/[brand]/[model]/[detailsproduct].js
@@ -0,0 +1,11 @@
+import DetailsPageProduct from '../../../../components/DetailsPageProduct';
+
+const DetailsProduct = () => {
+  return (
+    <div>
+      <DetailsPageProduct />
+    </div>
+  );
+};
+
+export default DetailsProduct;
