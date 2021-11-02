2ba654392d97c3969449cf2c0be35df6030ef4f6
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-02 10:20:16 +0100

    Remove unnecessary file

--- pages/apple/index.js
+++
@@ -1,9 +0,0 @@
-import Products from "../../components/Products";
-
-import { applePhones } from "../../uitils/data";
-
-const ApplePage = () => {
-  return <Products data={applePhones} />;
-};
-
-export default ApplePage;
