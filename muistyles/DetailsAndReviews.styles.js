4da56e628acfcec6def3c4efca1a91a66b245cb0
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-02 15:53:37 +0100

    Create styles for reviews part

---
+++ muistyles/DetailsAndReviews.styles.js
@@ -0,0 +1,17 @@
+export const tabelWrapperStyles = {
+  margin: '0 24px',
+  overflowX: 'auto !important',
+  scrollbarWidth: 'thin',
+  scrollbarColor: 'rgb(240, 239, 239)',
+  '&::-webkit-scrollbar': {
+    width: '3px',
+    height: '3px',
+  },
+  '&::-webkit-scrollbar-track': {
+    backgroundColor: 'rgb(240, 239, 239)',
+  },
+  '&::-webkit-scrollbar-track': {
+    backgroundColor: '#bdb6b6',
+    borderRadius: '3px',
+  },
+};
