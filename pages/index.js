1609a0305c916ca10f1d775fcba916a413458b78
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-02 10:16:08 +0100

    Fetch brands data from database

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
-import Layout from "../components/Layout";
-import BrandsProducts from "../components/BrandsProducts";
+import Layout from '../components/Layout';
+import BrandsProducts from '../components/BrandsProducts';
 
-const Home = () => {
+const Home = ({ brands }) => {
   return (
     <Layout>
-      <BrandsProducts />
+      <BrandsProducts brands={brands} />
     </Layout>
   );
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3000/api/v1/brands');
+  const data = await response.json();
+
+  return {
+    props: {
+      brands: data,
+    },
+  };
+}
