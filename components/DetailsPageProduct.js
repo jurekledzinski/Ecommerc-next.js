5967fc0ca69d9b0905a3e7005ae6b4d5b803d67c
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-02 10:05:44 +0100

    Create Details page product component

---
+++ components/DetailsPageProduct.js
@@ -0,0 +1,42 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@mui/material';
+import { useRoutesHook } from '../customHooks/useRoutesHook';
+import BreadCramps from './BreadCramps';
+import SliderThumbnails from './SliderThumbnails';
+import DescriptionProduct from './DescriptionProduct';
+
+import {
+  containerDetailsStyles,
+  leftSideStyles,
+  rightSlideStyles,
+  Section,
+} from '../muistyles/DetailsPageProduct.styles';
+
+const DetailsPageProduct = () => {
+  const { endpoints } = useRoutesHook();
+  console.log(endpoints, ' details product page breadcramps');
+
+  return (
+    <Section>
+      <BreadCramps endpoints={endpoints} />
+      <Grid container spacing={2} sx={containerDetailsStyles}>
+        <Grid item xs={12} sm={6} sx={leftSideStyles}>
+          <SliderThumbnails />
+        </Grid>
+        <Grid item xs={12} sm={6} sx={rightSlideStyles}>
+          <DescriptionProduct />
+        </Grid>
+      </Grid>
+    </Section>
+  );
+};
+
+export default DetailsPageProduct;
---
+++ muistyles/DetailsPageProduct.styles.js
@@ -0,0 +1,28 @@
+import { styled } from '@mui/material/styles';
+
+export const Section = styled('section')(({ theme }) => ({
+  maxWidth: '1200px',
+  margin: '0 auto',
+  [theme.breakpoints.up('xs')]: {
+    minHeight: 'calc(100vh - 90px)',
+  },
+  [theme.breakpoints.up('sm')]: {
+    minHeight: 'calc(100vh - 139px)',
+  },
+}));
+
+export const containerDetailsStyles = {
+  width: 'initial',
+  padding: (theme) => theme.spacing(3),
+  marginLeft: 'initial',
+};
+
+export const leftSideStyles = {
+  paddingRight: { sm: '16px' },
+  paddingLeft: 'initial !important',
+};
+
+export const rightSlideStyles = {
+  padding: { sm: '0 16px 16px 16px' },
+  paddingLeft: { xs: 'initial !important', sm: '16px' },
+};
