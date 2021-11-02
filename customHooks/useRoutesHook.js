8fee549fd7523b5d7daeeb630e6d7695349e5f90
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-02 10:12:07 +0100

    Create custom hook to prepare data to display in bread cramps

---
+++ customHooks/useRoutesHook.js
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/router';
+
+export const useRoutesHook = () => {
+  const router = useRouter();
+  const baseurl = [{ name: 'Home', url: '/' }];
+  const queries = router.query;
+  delete queries.category;
+  delete queries.id;
+  delete queries.model;
+  const indexModel = router.asPath.indexOf('model');
+  const condition =
+    indexModel === -1 ? router.asPath : router.asPath.slice(0, indexModel - 1);
+  let arrQueries = Object.values(queries).map((item, index) => {
+    return {
+      name: item.charAt(0).toUpperCase() + item.substr(1).replace(/\-/g, ' '),
+      url: index === 0 ? condition : router.asPath,
+    };
+  });
+  return { endpoints: baseurl.concat(arrQueries) };
+};
