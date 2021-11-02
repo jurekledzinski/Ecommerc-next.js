b5e5b6068dcb2de2e9f85629336a56a9c9bb2d48
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-02 10:03:43 +0100

    Create BreadCramps component and style it

---
+++ components/BreadCramps.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Box, Breadcrumbs } from '@mui/material';
+import {
+  BreadLink,
+  breadCrampsWrapperStyles,
+} from '../muistyles/BreadCramps.styles';
+
+const BreadCramps = ({ endpoints }) => {
+  const [breadCrampIndex, setBreadCrampIndex] = useState(null);
+
+  useEffect(() => {
+    setBreadCrampIndex(endpoints.length - 1);
+  }, [endpoints]);
+
+  return (
+    <Box sx={breadCrampsWrapperStyles}>
+      <Breadcrumbs aria-label="breadcrumb">
+        {endpoints.map((item, index) => (
+          <Link href={item.url} key={index}>
+            <BreadLink
+              breadcrampindex={breadCrampIndex}
+              index={index}
+              onClick={() => setBreadCrampIndex(index)}
+            >
+              {item.name}
+            </BreadLink>
+          </Link>
+        ))}
+      </Breadcrumbs>
+    </Box>
+  );
+};
+
+export default BreadCramps;
---
+++ muistyles/BreadCramps.styles.js
@@ -0,0 +1,23 @@
+import { styled } from '@mui/material/styles';
+
+export const BreadLink = styled('a')(({ breadcrampindex, index, theme }) => ({
+  color:
+    breadcrampindex === index
+      ? theme.palette.primary.light
+      : theme.palette.text.primary,
+  textDecoration: 'none',
+  cursor: 'pointer',
+  [theme.breakpoints.up('xs')]: {
+    fontSize: '1.4rem',
+  },
+  [theme.breakpoints.up('sm')]: {
+    fontSize: '1.6rem',
+  },
+  transition: '0.2s ease',
+  ':hover': { color: '#e5e5e5' },
+}));
+
+export const breadCrampsWrapperStyles = {
+  paddingLeft: (theme) => theme.spacing(3),
+  marginTop: (theme) => theme.spacing(3),
+};
