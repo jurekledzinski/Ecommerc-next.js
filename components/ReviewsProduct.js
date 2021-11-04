e6989083870ed337920787c8819c5d579756e27b
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-04 11:13:45 +0100

    Add reviews sections and style it

--- components/ReviewsProduct.js
+++ components/ReviewsProduct.js
@@ -1,18 +1,36 @@
 import React, { useState } from 'react';
-
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
 import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 
 import {
+  avatarReviewsOneStyles,
+  avatarReviewsTwoStyles,
+  boxNextToAvatarStyles,
+  boxInPaperReviewsStyles,
+  boxWrapperAvatarStyles,
   commentsTitleStyles,
   formReviewStyles,
+  iconBtnThumbUp,
+  nameUserReviewStyles,
+  paperReviewsStyles,
+  ratesReviewProductOneStyles,
   reviewTitleStyles,
+  reviewStyles,
+  ThumbUpBadge,
+  thumbUpIconStyles,
+  timeReviewStyles,
   wrapperRatingReviewStyles,
 } from '../muistyles/ReviewsProduct.styles';
 
@@ -23,131 +41,134 @@ const ReviewsProduct = () => {
     e.preventDefault();
   };
 
+  const handleLikeReview = () => {
+    console.log('click thumb up review');
+  };
+
   return (
     <Box>
-      <Box
-        component="form"
-        onSubmit={(e) => handleSubmitAddReview(e)}
-        sx={formReviewStyles}
-      >
-        <Typography variant="h5" sx={reviewTitleStyles}>
-          Write your review
-        </Typography>
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Review"
-          multiline
-          rows={6}
-          value={review}
-          onChange={(e) => setReview(e.target.value)}
-        />
-        <Stack spacing={1} sx={wrapperRatingReviewStyles}>
-          <Rating
-            name="half-rating"
-            defaultValue={0}
-            precision={0.5}
-            size="large"
-          />
-        </Stack>
-        <Button variant="contained" size="large">
-          Write review
-        </Button>
-      </Box>
-      <Box>
-        <Typography variant="h5" sx={commentsTitleStyles}>
-          Reviews (1)
-        </Typography>
-        <Paper
-          variant="outlined"
-          square
-          component="article"
-          sx={{
-            position: 'relative',
-            padding: '10px',
-            display: 'flex',
-            flexDirection: { xs: 'column', sm: 'row' },
-          }}
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
         >
+          <Typography variant="h5" sx={reviewTitleStyles}>
+            Write your review
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
           <Box
-            sx={{
-              width: '80px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'flex-start',
-            }}
+            component="form"
+            onSubmit={(e) => handleSubmitAddReview(e)}
+            sx={formReviewStyles}
           >
-            <Avatar
-              alt="Remy Sharp"
-              src="https://cdn.pixabay.com/photo/2021/09/24/22/05/woman-6653634_960_720.jpg"
-              sx={{
-                width: '40px',
-                height: '40px',
-                display: { xs: 'none', sm: 'block' },
-              }}
-            ></Avatar>
+            <TextField
+              id="outlined-multiline-flexible"
+              label="Review"
+              multiline
+              rows={6}
+              value={review}
+              onChange={(e) => setReview(e.target.value)}
+            />
+            <Stack spacing={1} sx={wrapperRatingReviewStyles}>
+              <Rating
+                name="half-rating"
+                defaultValue={0}
+                precision={0.5}
+                size="large"
+              />
+            </Stack>
+            <Button variant="contained" size="large">
+              Write review
+            </Button>
           </Box>
-          <Box
-            sx={{
-              width: { xs: 'initial', sm: 'calc(100% - 80px)' },
-            }}
+        </AccordionDetails>
+      </Accordion>
+      <Box>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
           >
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: { xs: 'space-around', sm: 'initial' },
-                alignItems: { xs: 'center', sm: 'flex-end' },
-                marginBottom: '10px',
-                width: { xs: '200px', sm: 'initial' },
-              }}
-            >
+            <Typography variant="h5" sx={commentsTitleStyles}>
+              Reviews (1)
+            </Typography>
+          </AccordionSummary>
+          <Paper
+            variant="outlined"
+            square
+            component="article"
+            sx={paperReviewsStyles}
+          >
+            <Box sx={boxInPaperReviewsStyles}>
               <Avatar
                 alt="Remy Sharp"
                 src="https://cdn.pixabay.com/photo/2021/09/24/22/05/woman-6653634_960_720.jpg"
-                sx={{
-                  width: '40px',
-                  height: '40px',
-                  display: { xs: 'block', sm: 'none' },
-                }}
+                sx={avatarReviewsOneStyles}
               ></Avatar>
-              <h5 style={{ fontSize: '1.4rem', marginRight: '5px' }}>
-                VamosRM
-              </h5>
-              <Box
-                component="span"
-                sx={{ color: '#aeaeae', fontSize: '1.2rem' }}
-              >
-                Today 18:36:32
-              </Box>
+              <Rating
+                name="half-rating"
+                defaultValue={3.5}
+                precision={0.5}
+                size="small"
+                sx={ratesReviewProductOneStyles}
+                readOnly
+              />
             </Box>
-            <Typography
-              variant="body2"
-              sx={{ fontSize: { xs: '1.4rem', sm: '1.6rem', fontWeight: 300 } }}
-            >
-              I would recommend to apply scrollbar styles only for the specific
-              container, cause @Global may take rendering time to apply on the
-              All elements. This works fine as for me.I would recommend to apply
-              scrollbar styles only for the specific container, cause @Global
-              may take rendering time to apply on the All elements. This works
-              fine as for me I would recommend to apply scrollbar styles only
-              for the specific container, cause @Global may take rendering time
-              to apply on the All elements. This works fine as for me.I would
-              recommend to apply scrollbar styles only for the specific
-              container, cause @Global may take rendering time to apply on the
-              All elements. This works fine as for me I would recommend to apply
-              scrollbar styles only for the specific container, cause @Global
-              may take rendering time to apply on the All elements. This works
-              fine as for me.I would recommend to apply scrollbar styles only
-              for the specific container, cause @Global may take rendering time
-              to apply on the All elements. This works fine as for me I would
-              recommend to apply scrollbar styles only for the specific
-              container, cause @Global may take rendering time to apply on the
-              All elements. This works fine as for me.I would recommend to apply
-              scrollbar styles only for the specific container, cause @Global
-              may take rendering time to apply on the All elements. This works
-              fine as for me
-            </Typography>
-          </Box>
-        </Paper>
+            <Box sx={boxNextToAvatarStyles}>
+              <Box sx={boxWrapperAvatarStyles}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: { xs: 'column', sm: 'row' },
+                    alignItems: 'flex-start',
+                    width: '100%',
+                  }}
+                >
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="https://cdn.pixabay.com/photo/2021/09/24/22/05/woman-6653634_960_720.jpg"
+                    sx={avatarReviewsTwoStyles}
+                  ></Avatar>
+                  <Typography style={nameUserReviewStyles}>VamosRM</Typography>
+                  <Box component="span" sx={timeReviewStyles}>
+                    Today 18:36:32
+                  </Box>
+                </Box>
+              </Box>
+              <Rating
+                name="half-rating"
+                defaultValue={3.5}
+                precision={0.5}
+                size="small"
+                sx={{ display: { xs: 'flex', sm: 'none' }, margin: '5px 0' }}
+                readOnly
+              />
+              <Typography variant="body2" sx={reviewStyles}>
+                I would recommend to apply scrollbar styles only for the
+                specific container, cause @Global may take rendering time to
+                apply on the All elements. This works fine as for me.I would
+                recommend to apply scrollbar styles only for the specific
+                container, cause @Global may take rendering time to apply on the
+                All elements. This works fine as for me I would recommend to
+                apply scrollbar styles only for the specific container, cause
+                @Global may take rendering time to apply on the All elements.
+                This works fine as for me.
+              </Typography>
+              <IconButton
+                aria-label="delete"
+                sx={iconBtnThumbUp}
+                onClick={handleLikeReview}
+              >
+                <ThumbUpBadge badgeContent={1} max={99}>
+                  <ThumbUpAltIcon sx={thumbUpIconStyles} />
+                </ThumbUpBadge>
+              </IconButton>
+            </Box>
+          </Paper>
+        </Accordion>
       </Box>
     </Box>
   );
