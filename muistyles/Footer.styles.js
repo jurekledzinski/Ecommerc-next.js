c3d64a8b548dd5a4b89ea48554d219924a0c5974
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-04 11:15:20 +0100

    Change font size of footer text

--- muistyles/Footer.styles.js
+++ muistyles/Footer.styles.js
@@ -1,21 +1,18 @@
-import { alpha, styled } from "@mui/material/styles";
+import { alpha, styled } from '@mui/material/styles';
 
-export const Footer = styled("footer")(({ theme }) => ({
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  height: "50px",
-  backgroundColor: "#444444",
+export const Footer = styled('footer')(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '50px',
+  backgroundColor: '#444444',
 }));
 
-export const FooterText = styled("p")(({ theme }) => ({
-  color: "white",
+export const FooterText = styled('p')(({ theme }) => ({
+  color: 'white',
   fontWeight: 300,
   letterSpacing: 1,
-  [theme.breakpoints.up("xs")]: {
-    fontSize: "14px",
-  },
-  [theme.breakpoints.up("sm")]: {
-    fontSize: "16px",
+  [theme.breakpoints.up('xs')]: {
+    fontSize: '14px',
   },
 }));
