09ab732439d5f3a9f7f8ab7f351702a1bb86fd3e
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-11 14:22:13 +0100

    Display table specification for each product

--- components/TableDetailsProduct.js
+++ components/TableDetailsProduct.js
@@ -1,3 +1,7 @@
+import React, { Fragment, useContext } from 'react';
+
+import { StoreContext } from '../utils/store';
+
 import {
   TableWrapper,
   TdTable,
@@ -5,22 +9,34 @@ import {
 } from '../muistyles/TableDetailsProduct.styles';
 
 const TableDetailsProduct = () => {
+  const { stateDetailsProduct } = useContext(StoreContext);
+  const { details } = stateDetailsProduct;
+
   return (
     <TableWrapper>
       <tbody>
-        <TrTable>
-          <TdTable colSpan="2" className="feature-name">
-            <h4>Basic information</h4>
-          </TdTable>
-        </TrTable>
-        <TrTable>
-          <TdTable>Brand</TdTable>
-          <TdTable>XIAOMI</TdTable>
-        </TrTable>
-        <TrTable>
-          <TdTable>Colors</TdTable>
-          <TdTable>Black</TdTable>
-        </TrTable>
+        {details &&
+          details.map((item1, index) => (
+            <Fragment key={index}>
+              <TrTable>
+                <TdTable colSpan="2" className="feature-name">
+                  <h4>{item1.title}</h4>
+                </TdTable>
+              </TrTable>
+              {Object.keys(item1)
+                .filter((item2) => item2 !== 'title')
+                .map((item3) => (
+                  <TrTable key={item3}>
+                    <TdTable>
+                      {item3.match(/2|3|4|5/g)
+                        ? item3 + 'gband'
+                        : item3.charAt(0).toUpperCase() + item3.substring(1)}
+                    </TdTable>
+                    <TdTable>{item1[item3]}</TdTable>
+                  </TrTable>
+                ))}
+            </Fragment>
+          ))}
       </tbody>
     </TableWrapper>
   );
