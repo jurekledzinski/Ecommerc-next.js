1e78e0029e112475410171f71f9ea8a7f674b6e1
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-11 14:17:20 +0100

    Create controlCart function to control operations on cart

---
+++ helpers/carthelpers.js
@@ -0,0 +1,84 @@
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_ON_STOCK_CART_PRODUCT,
+  UPDATE_PRODUCTS_BRAND_ON_STOCK,
+  UPDATE_ON_STOCK_PRODUCT_DETAILS,
+  UPDATE_TOTAL_PRICE_CART_PRODUCT,
+  UPDATE_TOTAL_CART_AMOUNT,
+  UPDATE_TOTAL_CART_PRICE,
+} from '../utils/constants';
+
+export const controlCart = (
+  data,
+  idProduct,
+  stateCart,
+  dispatchCart,
+  addedAmountToCart = 1,
+  operator = true,
+  removeProductFlag = false,
+  disptachProductsBrand,
+  dispatchDetailsProduct
+) => {
+  const checkCart = stateCart.products.find((item) => item._id === idProduct);
+
+  if (!checkCart && !removeProductFlag) {
+    dispatchCart({
+      type: ADD_TO_CART,
+      product: data,
+      addedAmountToCart: addedAmountToCart,
+    });
+  } else if (checkCart && !removeProductFlag) {
+    dispatchCart({
+      type: UPDATE_ON_STOCK_CART_PRODUCT,
+      controlStock: operator,
+      idProduct,
+      addedAmountToCart: addedAmountToCart,
+    });
+    dispatchCart({
+      type: UPDATE_TOTAL_PRICE_CART_PRODUCT,
+      controlStock: operator,
+      idProduct,
+      addedAmountToCart: addedAmountToCart,
+    });
+    dispatchCart({
+      type: UPDATE_TOTAL_CART_AMOUNT,
+      controlStock: operator,
+      addedAmountToCart: addedAmountToCart,
+    });
+    dispatchCart({
+      type: UPDATE_TOTAL_CART_PRICE,
+      controlStock: operator,
+      product: data,
+      addedAmountToCart: addedAmountToCart,
+    });
+  }
+
+  if (removeProductFlag && checkCart) {
+    dispatchCart({
+      type: REMOVE_FROM_CART,
+      idProduct,
+    });
+    disptachProductsBrand({
+      type: UPDATE_PRODUCTS_BRAND_ON_STOCK,
+      productId: idProduct,
+      amountOnStock: checkCart.amount,
+    });
+    dispatchDetailsProduct({
+      type: UPDATE_ON_STOCK_PRODUCT_DETAILS,
+      productId: idProduct,
+      updateOnStock: checkCart.amount,
+    });
+    dispatchCart({
+      type: UPDATE_TOTAL_CART_AMOUNT,
+      controlStock: operator,
+      addedAmountToCart: checkCart.amount - checkCart.onStock,
+    });
+    dispatchCart({
+      type: UPDATE_TOTAL_CART_PRICE,
+      controlStock: operator,
+      product: data,
+      addedAmountToCart: checkCart.amount - checkCart.onStock,
+    });
+  }
+};
