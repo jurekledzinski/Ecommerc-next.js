2aff716753e3eb53782c7e601cb23fb49d6576fa
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-11 14:24:08 +0100

    Add hover effect on slider images to show magnifier

--- muistyles/SliderThumbnails.styles.js
+++ muistyles/SliderThumbnails.styles.js
@@ -1,11 +1,52 @@
 import { styled } from '@mui/material/styles';
 
+export const WrapperSlideMainThumbNails = styled('div')(({ theme }) => ({
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  cursor: 'pointer',
+
+  '&::after': {
+    content: '""',
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    right: '0',
+    bottom: '0',
+    backgroundColor: 'transparent',
+    fontSize: '80px',
+    transformOrigin: 'center',
+    transition: '0.3s linear',
+    opacity: '0',
+  },
+  '&:hover::after': {
+    content: '"\\f1ee"',
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    color: '#bababa',
+    fontSize: '5em',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: 'material-design-iconic-font',
+    transition: '0.3s linear',
+    opacity: '1',
+  },
+}));
+
+export const WrapperSliderThumbNails = styled('div')(({ theme }) => ({
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}));
+
 export const mainSliderOptions = {
   type: 'fade',
   fixedHeight: '300px',
   pagination: false,
   arrows: false,
-  cover: true,
+  classes: { slide: 'splide__slide your-class-arrows' },
 };
 
 export const thumbsOptions = {
@@ -16,7 +57,6 @@ export const thumbsOptions = {
   focus: 'center',
   focus: 'center',
   pagination: false,
-  cover: true,
   dragMinThreshold: {
     mouse: 4,
     touch: 10,
@@ -44,3 +84,15 @@ export const WrapperSliderModalStyles = styled('div')(({ theme }) => ({
     height: 'initial',
   },
 }));
+
+export const boxOverlayImageStyles = {
+  position: 'absolute',
+  top: '0',
+  left: '0',
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: (theme) => theme.palette.background.paper,
+};
