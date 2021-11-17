35a3f98a3f74dd119050e6ad4ef36a3a38723f72
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-17 15:54:15 +0100

    Validate and change form with react hook form

--- components/ForgetPassword.js
+++ components/ForgetPassword.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
-import { Button, Box, TextField, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import {
   boxStyles,
   boxFormStyles,
+  FormForgetMsgStyles,
   infoTextStyles,
   inputEmailStyles,
   submitButtonStyles,
@@ -11,8 +16,36 @@ import {
 
 const ForgetPassword = () => {
   const [email, setEmail] = useState('');
-  const handleSubmitForm = (e) => {
-    e.preventDefault();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    setEmail(data);
+  };
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm({
+    defaultValues: {
+      email: '',
+    },
+  });
+
+  useEffect(() => {
+    if (email) {
+      const defaultValues = {
+        email: '',
+      };
+      reset(defaultValues);
+      setEmail('');
+    }
+  }, [email, reset]);
+
+  const errorMessage = (errorMsg) => {
+    console.log(errorMsg);
+    return <FormForgetMsgStyles>{errorMsg}</FormForgetMsgStyles>;
   };
 
   return (
@@ -21,20 +54,45 @@ const ForgetPassword = () => {
         Forget Password
       </Typography>
 
-      <Box component="form" sx={boxFormStyles} onSubmit={handleSubmitForm}>
+      <Box
+        component="form"
+        sx={boxFormStyles}
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <Typography variant="body2" sx={infoTextStyles}>
           Send email to change password
         </Typography>
-        <TextField
-          id="outlined-basic"
-          label="Email"
-          variant="outlined"
-          size="small"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          sx={inputEmailStyles}
+        {errors.email && errorMessage(errors.email.message)}
+        <Controller
+          name="email"
+          control={control}
+          rules={{
+            required: {
+              value: true,
+              message: 'Email is required',
+            },
+            pattern: {
+              value: /^\S+@\S+\.\S+$/,
+              message: "Email isn't valid",
+            },
+          }}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              size="small"
+              sx={inputEmailStyles}
+            />
+          )}
         />
-        <Button variant="contained" size="large" sx={submitButtonStyles}>
+        <Button
+          variant="contained"
+          size="large"
+          sx={submitButtonStyles}
+          type="submit"
+        >
           Change password
         </Button>
       </Box>
