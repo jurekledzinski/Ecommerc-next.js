0b8bffe40d1ece66cf51e7d5c3302e0660be8470
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-17 16:01:01 +0100

    Add functionlity to refresh token in details product page

--- pages/[category]/[brand]/[model]/[detailsproduct].js
+++ pages/[category]/[brand]/[model]/[detailsproduct].js
@@ -1,14 +1,23 @@
+import cookie from 'cookie';
 import React, { useEffect, useContext } from 'react';
 
 import { StoreContext } from '../../../../utils/store';
 import {
   FETCH_DETAILS_PRODUCT,
+  USER_LOGIN_DATA,
   UPDATE_ON_STOCK_PRODUCT_DETAILS,
 } from '../../../../utils/constants';
 import DetailsPageProduct from '../../../../components/DetailsPageProduct';
 
-const DetailsProduct = ({ detailsProduct }) => {
-  const { dispatchDetailsProduct, stateCart } = useContext(StoreContext);
+const DetailsProduct = ({ detailsProduct, user }) => {
+  const {
+    dispatchDetailsProduct,
+    dispatchLoginUser,
+    stateCart,
+    stateLoginUser,
+  } = useContext(StoreContext);
+
+  console.log(stateLoginUser, ' details product stateLoginUser');
 
   useEffect(() => {
     if (detailsProduct.name) {
@@ -29,6 +38,12 @@ const DetailsProduct = ({ detailsProduct }) => {
     });
   }, [detailsProduct]);
 
+  useEffect(() => {
+    if (Object.keys(user).length > 0) {
+      dispatchLoginUser({ type: USER_LOGIN_DATA, data: user });
+    }
+  }, [dispatchLoginUser, user]);
+
   return (
     <div>
       <DetailsPageProduct />
@@ -39,18 +54,66 @@ const DetailsProduct = ({ detailsProduct }) => {
 export default DetailsProduct;
 
 export async function getServerSideProps(context) {
+  console.log(context.req.cookies, 'products details getserver');
   const { category, brand, id } = context.query;
-  const response = await fetch(
+
+  const response1 = await fetch(
     `http://localhost:3000/api/v1/${category}/${brand}/${id}`
   );
-  const data = await response.json();
-  const [detailsProduct] = data;
 
-  //   console.log(detailsProduct, 'data check details page');
+  const response2 = await fetch(
+    'http://localhost:3000/api/v1/refresher-access',
+    {
+      method: 'PATCH',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'applications/json',
+        credentials: 'include',
+        cookie: JSON.stringify(context.req.cookies),
+      },
+    }
+  );
 
-  return {
-    props: {
-      detailsProduct,
-    },
-  };
+  if (response1.ok || response2.ok) {
+    const dataDetails = await response1.json();
+    const data = await response2.json();
+    const [detailsProduct] = dataDetails;
+    const { tokenAccess, user } = data;
+
+    if (data.user) {
+      context.res.setHeader(
+        'Set-Cookie',
+        cookie.serialize('refreshToken', user.tokenRefresh, {
+          httpOnly: true,
+          path: '/',
+        })
+      );
+    }
+
+    let dataChange;
+    if (data.tokenAccess) {
+      dataChange = {
+        tokenAccess: tokenAccess,
+        user: { _id: user._id, name: user.name },
+      };
+    }
+
+    return {
+      props: {
+        detailsProduct,
+        user: dataChange || {},
+      },
+    };
+  } else {
+    return {
+      props: {
+        detailsProduct: {},
+        user: {},
+        error: {
+          message: `Oops! ${response2.statusText}`,
+          statusCode: response2.status,
+        },
+      },
+    };
+  }
 }
