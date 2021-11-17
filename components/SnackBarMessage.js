04caf295303f3a16d52602c6d27764672076d57d
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-17 15:57:11 +0100

    Create component snackbar messages

---
+++ components/SnackBarMessage.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Alert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
+
+const SnackBarMessage = ({
+  errorMsg,
+  successMsg,
+  setErrorMsg,
+  setSuccessMsg,
+}) => {
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setErrorMsg('');
+    setSuccessMsg('');
+  };
+  return (
+    <>
+      {successMsg && (
+        <Snackbar
+          open={Boolean(successMsg)}
+          autoHideDuration={3000}
+          onClose={handleClose}
+        >
+          <Alert
+            onClose={handleClose}
+            severity="success"
+            variant="filled"
+            sx={{ width: '100%', fontSize: '1.4rem' }}
+          >
+            {successMsg}
+          </Alert>
+        </Snackbar>
+      )}
+      {errorMsg && (
+        <Snackbar
+          open={Boolean(errorMsg)}
+          autoHideDuration={3000}
+          onClose={handleClose}
+        >
+          <Alert
+            onClose={handleClose}
+            severity="error"
+            variant="filled"
+            sx={{ width: '100%', fontSize: '1.4rem' }}
+          >
+            {errorMsg}
+          </Alert>
+        </Snackbar>
+      )}
+    </>
+  );
+};
+
+export default SnackBarMessage;
