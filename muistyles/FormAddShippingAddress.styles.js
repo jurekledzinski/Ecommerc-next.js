492414bd0467ec302a2341ed13a37a348fe7a3fb
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-18 11:53:12 +0100

    Add styles to form add and update shipping data

---
+++ muistyles/FormAddShippingAddress.styles.js
@@ -0,0 +1,28 @@
+import { styled } from '@mui/material/styles';
+
+export const FormMsgShipProfile = styled('p')(({}) => ({
+  width: '95%',
+  fontSize: '1rem',
+  color: 'red',
+}));
+
+export const formProfileShipStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  width: '100%',
+};
+
+export const inputProfileShipStyles = {
+  width: { xs: '100%', sm: '100%' },
+  margin: '6px 0',
+};
+
+export const inputLastProfileShipStyles = {
+  width: { xs: '100%', sm: '100%' },
+  margin: '6px 0 20px 0',
+};
+
+export const addShipBtnStyles = {
+  width: { xs: '100%', sm: '100%' },
+};
