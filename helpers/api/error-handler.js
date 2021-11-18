559376847ced6ad29653f029808b33837bdb5a3d
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-18 11:56:09 +0100

    Update error handler api

--- helpers/api/error-handler.js
+++ helpers/api/error-handler.js
@@ -1,12 +1,22 @@
+const checkError = (error) => {
+  const result =
+    error.message.indexOf('duplicate key error') !== -1 ? true : false;
+  return result;
+};
+
 const errorHandler = (error, res) => {
-  console.log('error handling funkcja api', error);
+  console.log('error handling funkcja api', error.name);
   if (typeof error === 'string') {
-    // custom application error
     return res.status(400).json({ msgError: error });
   }
 
+  if (error.name === 'MongoServerError') {
+    return res.status(400).json({
+      msgError: checkError(error) ? 'Email already exist' : error.message,
+    });
+  }
+
   if (error.name === 'Unauthorized') {
-    // jwt authentication error
     return res.status(401).json({ msgError: error.message });
   }
 
@@ -16,9 +26,8 @@ const errorHandler = (error, res) => {
     });
   }
 
-  // default to 500 server error
   return res.status(500).json({
-    msgError: error.message,
+    msgError: 'Something went wrong. Please try later.',
   });
 };
 
