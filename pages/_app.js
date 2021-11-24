2dcce0a01f096cf26268319c6dbd30ce53679441
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-24 21:12:52 +0100

    Add handling errors pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Error from 'next/error';
 import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import '../styles/globals.css';
@@ -8,7 +9,7 @@ import {
   htmlStyles,
 } from '../muistyles/Global.styles';
 import GlobalStyles from '@mui/material/GlobalStyles';
-import { Box } from '@mui/material';
+import Box from '@mui/material/Box';
 
 import StoreProvider from '../utils/store';
 
@@ -26,6 +27,8 @@ import NavBar from '../components/NavBar';
 import FooterDown from '../components/FooterDown';
 import AsideDrawer from '../components/AsideDrawer';
 import ThemeMui from '../components/ThemeMui';
+import NotFoundPage from './404';
+import ServerErrorPage from './500';
 
 function MyApp({ Component, pageProps }) {
   const [loaded, setLoaded] = useState(false);
@@ -33,6 +36,55 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     setLoaded(true);
   }, []);
+
+  if (pageProps.error) {
+    const { statusCode } = pageProps.error;
+    switch (true) {
+      case statusCode >= 400 && statusCode < 500:
+        return (
+          <NotFoundPage
+            statusCode={pageProps.error.statusCode}
+            title={pageProps.error.message}
+          />
+        );
+      case statusCode >= 500 && statusCode < 600:
+        return (
+          <ServerErrorPage
+            statusCode={pageProps.error.statusCode}
+            title={pageProps.error.message}
+          />
+        );
+      default:
+        return (
+          <NotFoundPage
+            statusCode={pageProps.error.statusCode}
+            title={pageProps.error.message}
+          />
+        );
+    }
+  }
+
+  if (Component.getLayout) {
+    return Component.getLayout(
+      <StoreProvider>
+        <ThemeMui>
+          {loaded && (
+            <Box sx={appWrapperStyles}>
+              <Head>
+                <title>Shoppy</title>
+                <meta
+                  name="description"
+                  content="Buy phones, watches, tablets in good and cheap price"
+                />
+              </Head>
+              {loaded && <Component {...pageProps} />}
+              {inputGlobalStyles}
+            </Box>
+          )}
+        </ThemeMui>
+      </StoreProvider>
+    );
+  }
 
   return (
     <StoreProvider>
