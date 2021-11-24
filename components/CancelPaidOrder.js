3475922d5843ff6b98017ac351e25b1a7548c05e
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-24 20:49:31 +0100

    Create cancel paid order component and style it

---
+++ components/CancelPaidOrder.js
@@ -0,0 +1,78 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+import {
+  CircleCancelElementOne,
+  CircleCancelElementTwo,
+  DivIconCancel,
+  DivCancel,
+  GElementCancelSvg,
+  PolylineCancelElement,
+  rediretCancelBtn,
+  SectionCancelPage,
+  titleCancelPageStyles,
+} from '../muistyles/CancelPaidOrder.styles';
+
+const CancelPaidOrder = () => {
+  const router = useRouter();
+
+  const handleBackHomePage = () => {
+    router.push('/');
+  };
+
+  return (
+    <SectionCancelPage>
+      <Box>
+        <DivCancel className="animation-ctn">
+          <DivIconCancel className="icon icon--order-success svg">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="154px"
+              height="154px"
+            >
+              <GElementCancelSvg>
+                <CircleCancelElementOne
+                  className="circle"
+                  cx="75"
+                  cy="75"
+                  r="60"
+                ></CircleCancelElementOne>
+                <CircleCancelElementTwo
+                  id="colored"
+                  cx="75"
+                  cy="75"
+                  r="60"
+                ></CircleCancelElementTwo>
+                <PolylineCancelElement
+                  className="polyline"
+                  points="43.5,77.8  112.2,77.8 "
+                />
+              </GElementCancelSvg>
+            </svg>
+          </DivIconCancel>
+        </DivCancel>
+        <Typography
+          id="transition-modal-title"
+          variant="h4"
+          sx={titleCancelPageStyles}
+        >
+          Your order has been canceled successfully
+        </Typography>
+        <Button
+          variant="contained"
+          size="large"
+          sx={rediretCancelBtn}
+          type="submit"
+          onClick={handleBackHomePage}
+        >
+          Back to home page
+        </Button>
+      </Box>
+    </SectionCancelPage>
+  );
+};
+
+export default CancelPaidOrder;
---
+++ muistyles/CancelPaidOrder.styles.js
@@ -0,0 +1,91 @@
+import { styled } from '@mui/material/styles';
+import { keyframes } from '@mui/system';
+
+const checkmarkCancel = keyframes`
+0% {
+    stroke-dashoffset: 100px
+}
+100% {
+    stroke-dashoffset: 0px
+}
+`;
+
+const checkmarkCircleCancel = keyframes`
+0% {
+    stroke-dashoffset: 480px 
+}
+100% {
+    stroke-dashoffset: 960px
+}
+`;
+
+const coloredCircleCancel = keyframes`
+0% {
+    opacity:0
+}
+100% {
+    opacity:100
+}
+`;
+
+export const CircleCancelElementOne = styled('circle')(({ theme }) => ({
+  strokeDasharray: '480px 480px',
+  strokeDashoffset: '960px',
+}));
+
+export const CircleCancelElementTwo = styled('circle')(({ theme }) => ({
+  fill: 'transparent',
+  strokeDasharray: '480px 480px',
+  strokeDashoffset: '960px',
+}));
+
+export const DivIconCancel = styled('section')(({ theme }) => ({
+  '& .polyline': {
+    WebkitAnimation: `${checkmarkCancel} 0.25s ease-in-out 0.7s backwards`,
+    animation: `${checkmarkCancel} 0.25s ease-in-out 0.7s backwards`,
+  },
+  '& .circle': {
+    WebkitAnimation: `${checkmarkCircleCancel} 0.6s ease-in-out backwards`,
+    animation: `${checkmarkCircleCancel} 0.6s ease-in-out backwards`,
+  },
+  '& #colored': {
+    WebkitAnimation: `${coloredCircleCancel} 0.6s ease-in-out 0.7s backwards`,
+    animation: `${checkmarkCircleCancel} 0.6s ease-in-out 0.7s backwards`,
+  },
+}));
+
+export const DivCancel = styled('div')(({ theme }) => ({
+  textAlign: 'center',
+}));
+
+export const GElementCancelSvg = styled('g')(({ theme }) => ({
+  fill: 'none',
+  stroke: '#FF4136',
+  strokeWidth: '7',
+}));
+
+export const PolylineCancelElement = styled('polyline')(({ theme }) => ({
+  strokeDasharray: '100px 100px',
+  strokeDashoffset: '200px',
+  stroke: '#FF4136',
+  strokeWidth: '7',
+}));
+
+export const rediretCancelBtn = {
+  display: 'block',
+  margin: '20px auto 0 auto',
+};
+
+export const SectionCancelPage = styled('section')(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  minHeight: '100vh',
+  padding: '20px',
+}));
+
+export const titleCancelPageStyles = {
+  marginTop: '10px',
+  textAlign: 'center',
+};
