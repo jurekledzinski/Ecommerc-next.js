5e848d85c594dfd2118f7663685d20bf2c2c845c
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-11-24 21:04:10 +0100

    Add stripe script to document

--- pages/_document.js
+++ pages/_document.js
@@ -19,6 +19,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&family=Oswald:wght@500&display=swap"
             rel="stylesheet"
           />
+          <script src="https://js.stripe.com/v3" async></script>
         </Head>
         <body>
           <Main />
