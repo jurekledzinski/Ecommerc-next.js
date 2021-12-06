69bf8b5458239fa94ae2b7ed510fe3e91d31c394
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-12-06 14:21:40 +0100

    Add styles for review product

--- muistyles/ReviewsProduct.styles.js
+++ muistyles/ReviewsProduct.styles.js
@@ -4,3 +4,11 @@ export const commentsTitleStyles = {
   fontFamily: 'Oswald,sans-serif',
   fontWeight: '500',
 };
+
+export const reviewNoCommentsStyles = {
+  marginTop: '10px',
+  color: (theme) => theme.palette.text.primary,
+  textTransform: 'uppercase',
+  fontFamily: 'Oswald,sans-serif',
+  fontWeight: '500',
+};
