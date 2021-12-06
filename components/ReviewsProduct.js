cf9f535c98a06fc03d4151671e0104f85eea86fa
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-12-06 14:17:25 +0100

    Show number of reviews of product, display name user when is logged in when is no reviews

--- components/ReviewsProduct.js
+++ components/ReviewsProduct.js
@@ -1,42 +1,52 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import Box from '@mui/material/Box';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
 
-import { commentsTitleStyles } from '../muistyles/ReviewsProduct.styles';
+import {
+  commentsTitleStyles,
+  reviewNoCommentsStyles,
+} from '../muistyles/ReviewsProduct.styles';
 
 import ReviewForm from './ReviewForm';
 import EditReviewForm from './EditReviewForm';
 import SingleReview from './SingleReview';
 
-const ReviewsProduct = () => {
-  const [showEditForm, setShowEditForm] = useState(false);
+import { StoreContext } from '../utils/store';
 
-  const handleShowEditForm = () => {
-    console.log('click open edit');
-    setShowEditForm((prevValue) => !prevValue);
-  };
+const ReviewsProduct = () => {
+  const { stateReviews, stateLoginUser } = useContext(StoreContext);
+  const { user } = stateLoginUser;
+  console.log(stateLoginUser, ' ReviewsProduct');
+
+  console.log(stateReviews, 'stateReviews reviews products');
 
   return (
     <Box>
       <ReviewForm />
-      <Box>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography variant="h5" sx={commentsTitleStyles}>
-              Reviews (1)
-            </Typography>
-          </AccordionSummary>
-          <SingleReview handleShowEditForm={handleShowEditForm} />
-          {showEditForm && <EditReviewForm />}
-        </Accordion>
-      </Box>
+      {stateReviews.length > 0 ? (
+        <Box>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography variant="h5" sx={commentsTitleStyles}>
+                Reviews {`(${stateReviews.length})`}
+              </Typography>
+            </AccordionSummary>
+            <SingleReview />
+          </Accordion>
+        </Box>
+      ) : (
+        <Typography variant="h5" sx={reviewNoCommentsStyles}>
+          No reviews. Write first review{' '}
+          {Object.keys(stateLoginUser).length > 0 && user.name}
+        </Typography>
+      )}
     </Box>
   );
 };
