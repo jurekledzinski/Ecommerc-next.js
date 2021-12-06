767c5e3d13fcfa7a165000005b7c50b533fa472b
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-12-06 21:52:12 +0100

    Add functions to check tokens url and access change password

--- helpers/api/auth-helper.js
+++ helpers/api/auth-helper.js
@@ -7,3 +7,16 @@ export const isAuth = (req) => {
   const { idUser } = jwt.verify(token, process.env.JWT_SECRET_ACCESS);
   return idUser;
 };
+
+export const isAuthUser = (token) => {
+  const { email } = jwt.verify(token, process.env.JWT_SECRET_FORGET_PASSWORD);
+  return email;
+};
+
+export const isAccessTokenChangePassword = (token) => {
+  const { email } = jwt.verify(
+    token,
+    process.env.JWT_SECRET_TOKEN_ACCESS_CHANGE_PASSWORD
+  );
+  return email;
+};
