b43b88bbca77c443879d82c282f463a99a449211
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-12-06 22:04:30 +0100

    Display form to change password when tokens are valid and page was not refresh otherwise display no permissions page or when tokens expire page  not found

---
+++ pages/change_password/[tokenUrl].js
@@ -0,0 +1,63 @@
+import { useRouter } from 'next/router';
+
+import ChangePasswordForm from '../../components/ChangePasswordForm';
+import NoPermissionsPage from '../../components/NoPermissionsPage';
+
+const ChangePasswordPage = ({ tokenAccessChange, tokenUrl }) => {
+  const router = useRouter();
+
+  if (router.query.tokenUrl !== tokenUrl) {
+    return <NoPermissionsPage />;
+  }
+
+  return <ChangePasswordForm urlCode={tokenAccessChange} />;
+};
+export default ChangePasswordPage;
+
+ChangePasswordPage.getLayout = function PageLayout(page) {
+  return <>{page}</>;
+};
+
+export async function getServerSideProps(context) {
+  const urlToken = context.query.tokenUrl;
+
+  const response1 = await fetch(
+    `http://localhost:3000/api/v1/register?tokenUrl=${urlToken}`
+  );
+
+  if (response1.ok) {
+    const dataToken = await response1.json();
+
+    if (dataToken.data === urlToken) {
+      const response2 = await fetch(
+        `http://localhost:3000/api/v1/register?tokenUrl=${urlToken}`,
+        {
+          method: 'PATCH',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      await response2.json();
+    }
+
+    return {
+      props: {
+        tokenAccessChange: dataToken.dataAccess || '',
+        tokenUrl: dataToken.data || '',
+      },
+    };
+  } else {
+    return {
+      props: {
+        tokenAccessChange: '',
+        tokenUrl: '',
+        error: {
+          message: `Oops! ${response1.statusText}`,
+          statusCode: response1.status,
+        },
+      },
+    };
+  }
+}
