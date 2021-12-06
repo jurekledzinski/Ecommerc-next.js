dea3da5e7348b5dd20427c66b551c47135687ec9
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-12-06 14:24:52 +0100

    Create api to handel get, add, update, delete. Check who upvote review, count average rate product

---
+++ pages/api/v1/reviews.js
@@ -0,0 +1,133 @@
+import { connectDb } from '../../../utils/db';
+import Product from '../../../models/products';
+import Review from '../../../models/reviews';
+import errorHandler from '../../../helpers/api/error-handler';
+import { isAuth } from '../../../helpers/api/auth-helper';
+
+const likesUpdate = async (idUser, idReview) => {
+  const result = await Review.aggregate([
+    {
+      $project: {
+        isUserUpvote: {
+          $in: [idUser, '$likesUsers'],
+        },
+      },
+    },
+  ]);
+
+  const user = result.find((item) => item._id.toString() === idReview);
+  return user.isUserUpvote;
+};
+
+const updateProductRate = async (productId) => {
+  const resultReview = await Review.find({ productId: productId }).select([
+    '-_id',
+    'rate',
+  ]);
+
+  const initialValue = 0;
+  const amountRates = resultReview.length;
+  const sumRates = resultReview.reduce((prevValue, currentValue) => {
+    return parseFloat(prevValue) + parseFloat(currentValue.rate);
+  }, initialValue);
+
+  const averageRate = sumRates / amountRates;
+
+  let average = Number.isInteger(averageRate)
+    ? averageRate
+    : averageRate < Math.round(averageRate)
+    ? Math.round(averageRate) - 0.5
+    : Math.round(averageRate) + 0.5;
+
+  const resultProduct = await Product.findOneAndUpdate(
+    { _id: productId },
+    { rate: isNaN(averageRate) ? '0' : average.toString() },
+    {
+      new: true,
+      select: { rate: 1, _id: 0 },
+    }
+  );
+
+  return resultProduct.rate;
+};
+
+const updateReview = async (req, res, idReview, productId) => {
+  const { avatarImage, editRate, editReview, likes, name, userID } = req.body;
+  const numberLikes = likes === 0 || likes ? likes + 1 : likes;
+
+  const result = await Review.findOneAndUpdate(
+    { _id: idReview },
+    {
+      $addToSet: { likesUsers: userID },
+      avatarImage: avatarImage,
+      likes: numberLikes,
+      name: name,
+      rate: editRate,
+      review: editReview,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (result) {
+    const msg = 'Review updated';
+    const rateProduct = await updateProductRate(productId);
+    return res.status(200).json({ data: result, msgSuccess: msg, rateProduct });
+  }
+};
+
+const handler = connectDb(async (req, res) => {
+  const { aim, idReview, productId } = req.query;
+
+  try {
+    if (req.method === 'GET') {
+      const result = await Review.find({
+        productId: productId,
+      });
+      if (result) return res.status(200).json({ data: result });
+    } else if (req.method === 'POST') {
+      const idUser = isAuth(req);
+      const reviewCheck = await Review.find({
+        userId: idUser,
+        productId: productId,
+      });
+      if (reviewCheck.length === 0) {
+        const result = await Review.create(req.body);
+        const rateProduct = await updateProductRate(productId);
+        const msg = 'Review added';
+        return res
+          .status(200)
+          .json({ data: result, msgSuccess: msg, rateProduct });
+      } else {
+        throw 'You already review to this product';
+      }
+    } else if (req.method === 'PATCH') {
+      const idUser = isAuth(req);
+      switch (aim) {
+        case 'updateLikes':
+          const isUserUpVote = await likesUpdate(idUser, idReview);
+          if (isUserUpVote) {
+            throw 'You already upvote this review';
+          }
+        default:
+          await updateReview(req, res, idReview, productId);
+          break;
+      }
+    } else if (req.method === 'DELETE') {
+      const result = await Review.findByIdAndDelete({
+        _id: idReview,
+      });
+
+      if (result) {
+        const msg = 'Review removed';
+        const rateProduct = await updateProductRate(productId);
+        return res.status(200).json({ msgSuccess: msg, rateProduct });
+      }
+    }
+  } catch (error) {
+    errorHandler(error, res);
+  }
+});
+
+export default handler;
