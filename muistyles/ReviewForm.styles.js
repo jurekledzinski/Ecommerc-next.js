ac6fd859b794ae3565236d3f088809f8717a9b47
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-12-06 14:21:12 +0100

    Add styles to add review form

--- muistyles/ReviewForm.styles.js
+++ muistyles/ReviewForm.styles.js
@@ -1,3 +1,5 @@
+import { styled } from '@mui/material/styles';
+
 export const reviewTitleStyles = {
   color: (theme) => theme.palette.text.primary,
   textTransform: 'uppercase',
@@ -12,5 +14,27 @@ export const formReviewStyles = {
 
 export const wrapperRatingReviewStyles = {
   width: 'fit-content',
-  margin: '10px 0',
+  margin: '5px 0',
+};
+
+export const FormMsgReview = styled('p')(({}) => ({
+  width: '95%',
+  fontSize: '1rem',
+  color: 'red',
+}));
+
+export const FormMsgEditReview = styled('p')(({}) => ({
+  width: '95%',
+  fontSize: '1rem',
+  color: 'red',
+}));
+
+export const reviewLockerStyles = {
+  display: 'flex',
+  alignItems: 'center',
+  marginBottom: '5px',
+  color: (theme) => theme.palette.text.primary,
+  textTransform: 'uppercase',
+  fontFamily: 'Oswald,sans-serif',
+  fontWeight: '500',
 };
