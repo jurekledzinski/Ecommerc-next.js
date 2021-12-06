1ff684500ced868d0ce0e6467d62f5a579967948
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-12-06 14:15:34 +0100

    Add functionality to edit review and rating

--- components/EditReviewForm.js
+++ components/EditReviewForm.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
@@ -12,57 +12,201 @@ import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
 import {
+  FormMsgEditReview,
   formReviewStyles,
   reviewTitleStyles,
   wrapperRatingReviewStyles,
 } from '../muistyles/ReviewForm.styles';
 
-const EditReviewForm = () => {
-  const [review, setReview] = useState('');
+import { updateReview } from '../helpers/client/apiHelpers';
 
-  const handleSubmitAddReview = (e) => {
-    e.preventDefault();
+import {
+  CLOSE_EDIT_FORM,
+  UPDATE_REVIEW,
+  UPDATE_RATE_PRODUCT_DETAILS,
+} from '../utils/constants';
+
+import { StoreContext } from '../utils/store';
+
+import SnackBarMessage from './SnackBarMessage';
+
+const EditReviewForm = ({ idReview, userId }) => {
+  const {
+    dispatchEditForm,
+    dispatchDetailsProduct,
+    dispatchReview,
+    stateDetailsProduct,
+    stateLoginUser,
+    stateReviews,
+  } = useContext(StoreContext);
+  const { _id } = stateDetailsProduct;
+  const { tokenAccess, user } = stateLoginUser;
+  const [errorMsg, setErrorMsg] = useState('');
+  const [successMsg, setSuccessMsg] = useState('');
+  const idTimeout = useRef(null);
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    reset,
+    setError,
+  } = useForm({
+    defaultValues: {
+      editReview: '',
+      editRating: 0,
+    },
+  });
+
+  const onSubmit = async (data) => {
+    if (!parseFloat(data.editRating)) {
+      return setError('editRating', {
+        type: 'manual',
+        message: 'Rating is required',
+      });
+    }
+    console.log(user, 'comment edit');
+
+    const dataEdit = {
+      avatarImage: user.avatar,
+      editRate: data.editRating,
+      editReview: data.editReview,
+      name: user.name,
+    };
+
+    const result = await updateReview(
+      `http://localhost:3000/api/v1/reviews?idReview=${idReview}&productId=${_id}`,
+      dataEdit,
+      tokenAccess,
+      setErrorMsg
+    );
+
+    console.log(result, 'edit result from server');
+
+    if (Boolean(result) && result.msgSuccess) {
+      dispatchReview({
+        type: UPDATE_REVIEW,
+        idReview: idReview,
+        editRateData: result.data.rate,
+        editReviewData: result.data.review,
+      });
+      dispatchDetailsProduct({
+        type: UPDATE_RATE_PRODUCT_DETAILS,
+        rate: parseFloat(result.rateProduct),
+      });
+
+      setSuccessMsg(result.msgSuccess);
+      idTimeout.current = setTimeout(
+        () => dispatchEditForm({ type: CLOSE_EDIT_FORM }),
+        1000
+      );
+    }
   };
 
+  const errorMessage = (errorMsg) => {
+    return <FormMsgEditReview>{errorMsg}</FormMsgEditReview>;
+  };
+
+  const checkUser = () => {
+    let foundUser;
+    if (Boolean(user)) {
+      foundUser = userId === user._id;
+    }
+    return foundUser ? true : false;
+  };
+
+  useEffect(() => {
+    if (stateReviews.length > 0) {
+      console.log(idReview, 'idReview edit form id comment');
+      const reviewToEdit = stateReviews.find((item) => item._id === idReview);
+      console.log(reviewToEdit, 'reviewToEdit');
+      const defaultValues = {
+        editReview: reviewToEdit.review || '',
+        editRating: parseFloat(reviewToEdit.rate) || 0,
+      };
+      reset(defaultValues);
+    }
+  }, [stateReviews, reset]);
+
+  useEffect(() => {
+    return () => {
+      console.log('clear timeout');
+      clearTimeout(idTimeout.current);
+    };
+  }, []);
+
   return (
-    <Accordion>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="panel1a-content"
-        id="panel1a-header"
-      >
-        <Typography variant="h5" sx={reviewTitleStyles}>
-          Edit your review John
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Box
-          component="form"
-          onSubmit={(e) => handleSubmitAddReview(e)}
-          sx={formReviewStyles}
-        >
-          <TextField
-            id="outlined-multiline-flexible"
-            label="Edit review"
-            multiline
-            rows={6}
-            value={review}
-            onChange={(e) => setReview(e.target.value)}
-          />
-          <Stack spacing={1} sx={wrapperRatingReviewStyles}>
-            <Rating
-              name="half-rating"
-              defaultValue={0}
-              precision={0.5}
-              size="large"
-            />
-          </Stack>
-          <Button variant="contained" size="large">
-            Edit review
-          </Button>
-        </Box>
-      </AccordionDetails>
-    </Accordion>
+    <>
+      {checkUser() && (
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant="h5" sx={reviewTitleStyles}>
+              Edit your review {user.name}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Box
+              component="form"
+              onSubmit={handleSubmit(onSubmit)}
+              sx={formReviewStyles}
+            >
+              <SnackBarMessage
+                errorMsg={errorMsg}
+                successMsg={successMsg}
+                setErrorMsg={setErrorMsg}
+                setSuccessMsg={setSuccessMsg}
+              />
+              {errors.editReview && errorMessage(errors.editReview.message)}
+              <Controller
+                name="editReview"
+                control={control}
+                rules={{
+                  required: {
+                    value: true,
+                    message: 'Review is required',
+                  },
+                }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    id="outlined-multiline-flexible"
+                    label="Edit Review"
+                    multiline
+                    rows={6}
+                    sx={{ margin: '5px 0' }}
+                  />
+                )}
+              />
+              {errors.editRating && errorMessage(errors.editRating.message)}
+              <Controller
+                name="editRating"
+                control={control}
+                render={({ field }) => {
+                  let rate = { ...field, value: parseFloat(field.value) };
+                  return (
+                    <Stack spacing={1} sx={wrapperRatingReviewStyles}>
+                      <Rating
+                        {...rate}
+                        defaultValue={0}
+                        precision={0.5}
+                        size="large"
+                      />
+                    </Stack>
+                  );
+                }}
+              />
+              <Button variant="contained" size="large" type="submit">
+                Edit review
+              </Button>
+            </Box>
+          </AccordionDetails>
+        </Accordion>
+      )}
+    </>
   );
 };
 export default EditReviewForm;
