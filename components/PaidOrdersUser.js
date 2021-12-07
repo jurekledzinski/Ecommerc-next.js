d07376921cf0c0126d1bfdc174a5c996188cfd94
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-12-07 17:00:24 +0100

    Create component to display users paid orders and style it

--- components/PaidOrdersUser.js
+++ components/PaidOrdersUser.js
@@ -1,14 +1,282 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import DoNotDisturbIcon from '@mui/icons-material/DoNotDisturb';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Typography from '@mui/material/Typography';
 
 import { StoreContext } from '../utils/store';
 
-import { SectionPaidOrdersUser } from '../muistyles/PaidOrdersUser.styles';
+import {
+  boxLeftRightOrderPaidStyles,
+  boxWrapperContentOrdersPaidStyles,
+  boxPaidOrderWrapperTitlesStyles,
+  boxPaidOrdersWrapperStyles,
+  cardContentItemAmountOrderPaidStyles,
+  cardContentItemNameOrderPaidStyles,
+  cardContentPriceOrderPaidStyles,
+  cardContentWrapperOrderPaidStyles,
+  cardOrderPaidStyles,
+  iconNoOrdersPaidStyles,
+  iconTickOrdersPaidStyles,
+  dateOrderPaidTitleStyles,
+  deliveredOrderPaidTitleStyles,
+  mediaCardOrderPaidStyles,
+  orderPaidDetailsTextStyles,
+  orderPaidIdTitleStyles,
+  SectionPaidOrdersUser,
+  titleOrderPaidIconsStyles,
+  titlePaidOrdersStyles,
+  titleShippingOrdersPaidStyles,
+} from '../muistyles/PaidOrdersUser.styles';
 
 const PaidOrdersUser = () => {
   const { stateOrdersUser } = useContext(StoreContext);
-  const {} = stateOrdersUser;
+  const [expanded, setExpanded] = useState(false);
 
-  return <SectionPaidOrdersUser></SectionPaidOrdersUser>;
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <SectionPaidOrdersUser>
+      <Typography sx={titlePaidOrdersStyles} variant="h4">
+        User Orders
+      </Typography>
+      {stateOrdersUser.length > 0
+        ? stateOrdersUser.map((item, index) => (
+            <Box key={index} sx={boxPaidOrdersWrapperStyles}>
+              <Accordion
+                expanded={expanded === index}
+                onChange={handleChange(index)}
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1bh-content"
+                >
+                  <Box sx={boxPaidOrderWrapperTitlesStyles}>
+                    <Typography sx={orderPaidIdTitleStyles}>
+                      Order: {item.orderId}
+                    </Typography>
+                    <Typography sx={dateOrderPaidTitleStyles}>
+                      Date paid: {new Date(item.datePay).toLocaleString()}
+                    </Typography>
+                    <Typography sx={deliveredOrderPaidTitleStyles}>
+                      Delivered:{' '}
+                      {item.isDelivered ? (
+                        <CheckCircleOutlineIcon sx={iconTickOrdersPaidStyles} />
+                      ) : (
+                        <DoNotDisturbIcon sx={iconNoOrdersPaidStyles} />
+                      )}
+                    </Typography>
+                  </Box>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Box sx={boxWrapperContentOrdersPaidStyles}>
+                    <Box sx={boxLeftRightOrderPaidStyles}>
+                      <Typography
+                        variant="h6"
+                        sx={{
+                          ...titleShippingOrdersPaidStyles,
+                          marginBottom: '5px',
+                        }}
+                      >
+                        Shipping address:
+                      </Typography>
+                      <Typography sx={orderPaidDetailsTextStyles}>
+                        {item.name} {item.surname}
+                      </Typography>
+                      <Typography sx={orderPaidDetailsTextStyles}>
+                        {item.street}
+                      </Typography>
+                      <Typography sx={orderPaidDetailsTextStyles}>
+                        {item.city} {item.zipCode}
+                      </Typography>
+                      <Typography sx={orderPaidDetailsTextStyles}>
+                        {item.country}
+                      </Typography>
+                      <Typography
+                        variant="h6"
+                        sx={{
+                          ...titleShippingOrdersPaidStyles,
+                          margin: '5px 0',
+                        }}
+                      >
+                        Delivery details:
+                      </Typography>
+                      <Typography sx={orderPaidDetailsTextStyles}>
+                        Delivery method: {item.deliveryMethod}
+                      </Typography>
+                      <Typography sx={orderPaidDetailsTextStyles}>
+                        Delivery time: {item.timeDelivery}{' '}
+                        {`${item.timeDelivery > 1 ? 'days' : 'day'}`}
+                      </Typography>
+                      <Typography sx={orderPaidDetailsTextStyles}>
+                        Delivery price: {item.deliveryPrice.toFixed(2)}€
+                      </Typography>
+                      <Typography sx={titleOrderPaidIconsStyles}>
+                        Order delivered:
+                        {item.isDelivered ? (
+                          <CheckCircleOutlineIcon
+                            sx={iconTickOrdersPaidStyles}
+                          />
+                        ) : (
+                          <DoNotDisturbIcon sx={iconNoOrdersPaidStyles} />
+                        )}
+                      </Typography>
+                      <Typography
+                        variant="h6"
+                        sx={{
+                          ...titleShippingOrdersPaidStyles,
+                          margin: '5px 0',
+                        }}
+                      >
+                        Payment details:
+                      </Typography>
+                      <Typography sx={titleOrderPaidIconsStyles}>
+                        Order paid:
+                        {item.isPaid ? (
+                          <CheckCircleOutlineIcon
+                            sx={iconTickOrdersPaidStyles}
+                          />
+                        ) : (
+                          <DoNotDisturbIcon sx={iconNoOrdersPaidStyles} />
+                        )}
+                      </Typography>
+                      <Typography sx={orderPaidDetailsTextStyles}>
+                        Payment method: {item.paymentMethod}
+                      </Typography>
+                      <Typography
+                        variant="h6"
+                        sx={{
+                          ...titleShippingOrdersPaidStyles,
+                          margin: '5px 0',
+                        }}
+                      >
+                        Terms shop:
+                      </Typography>
+                      <Typography sx={titleOrderPaidIconsStyles}>
+                        Terms shop accepted:
+                        {item.termsConditions ? (
+                          <CheckCircleOutlineIcon
+                            sx={iconTickOrdersPaidStyles}
+                          />
+                        ) : (
+                          <DoNotDisturbIcon sx={iconNoOrdersPaidStyles} />
+                        )}
+                      </Typography>
+                    </Box>
+                    <Box sx={boxLeftRightOrderPaidStyles}>
+                      <Typography
+                        variant="h6"
+                        sx={titleShippingOrdersPaidStyles}
+                      >
+                        Ordered items:
+                      </Typography>
+                      <Box>
+                        {item?.cart?.products.map((item1, index) => (
+                          <Card key={index} sx={cardOrderPaidStyles}>
+                            <CardMedia
+                              sx={mediaCardOrderPaidStyles}
+                              component="img"
+                              image={item1.imagesSlider[0]}
+                              alt="image"
+                            />
+                            <CardContent sx={cardContentWrapperOrderPaidStyles}>
+                              <Typography
+                                variant="h6"
+                                sx={{
+                                  ...cardContentItemNameOrderPaidStyles,
+                                }}
+                              >
+                                {item1.name}
+                              </Typography>
+                              <Typography
+                                variant="body1"
+                                sx={{
+                                  ...cardContentItemNameOrderPaidStyles,
+                                  ...cardContentItemAmountOrderPaidStyles,
+                                }}
+                              >
+                                Amount: {item1.amount - item1.onStock}
+                              </Typography>
+                              <Typography
+                                variant="body1"
+                                sx={{
+                                  ...cardContentItemNameOrderPaidStyles,
+                                  ...cardContentPriceOrderPaidStyles,
+                                }}
+                              >
+                                Price: {item1.price}€
+                              </Typography>
+                            </CardContent>
+                          </Card>
+                        ))}
+                      </Box>
+                      <Typography
+                        variant="h6"
+                        sx={{
+                          ...titleShippingOrdersPaidStyles,
+                          fontWeight: '400',
+                        }}
+                      >
+                        Summary:
+                      </Typography>
+                      <Typography
+                        variant="h6"
+                        sx={{
+                          ...titleShippingOrdersPaidStyles,
+                          fontWeight: '300',
+                        }}
+                      >
+                        Total amount in cart: {item.cart.totalCartAmount}
+                      </Typography>
+                      <Typography
+                        variant="h6"
+                        sx={{
+                          ...titleShippingOrdersPaidStyles,
+                          fontWeight: '300',
+                        }}
+                      >
+                        Delivery price: {item.deliveryPrice.toFixed(2)}€
+                      </Typography>
+                      <Typography
+                        variant="h6"
+                        sx={{
+                          ...titleShippingOrdersPaidStyles,
+                          fontWeight: '300',
+                        }}
+                      >
+                        Total price:{' '}
+                        <Typography
+                          variant="h6"
+                          component="span"
+                          sx={{
+                            ...titleShippingOrdersPaidStyles,
+                            fontWeight: '400',
+                          }}
+                        >
+                          {(
+                            item.cart.totalCartPrice + item.deliveryPrice
+                          ).toFixed(2)}
+                          €
+                        </Typography>
+                      </Typography>
+                    </Box>
+                  </Box>
+                </AccordionDetails>
+              </Accordion>
+            </Box>
+          ))
+        : null}
+    </SectionPaidOrdersUser>
+  );
 };
 
 export default PaidOrdersUser;
--- muistyles/PaidOrdersUser.styles.js
+++ muistyles/PaidOrdersUser.styles.js
@@ -4,9 +4,153 @@ export const SectionPaidOrdersUser = styled('section')(({ theme }) => ({
   maxWidth: '1200px',
   margin: '0 auto',
   [theme.breakpoints.up('xs')]: {
-    minHeight: 'calc(100vh - 90px)',
+    minHeight: 'calc(100vh - 121px)',
   },
   [theme.breakpoints.up('sm')]: {
-    minHeight: 'calc(100vh - 139px)',
+    minHeight: 'calc(100vh - 131px)',
   },
 }));
+
+export const titlePaidOrdersStyles = {
+  margin: { xs: '16px 16px', sm: ' 16px 24px' },
+  color: (theme) => theme.palette.text.primary,
+  textTransform: 'uppercase',
+  fontFamily: 'Oswald, sans-serif',
+  fontWeight: '300',
+};
+
+export const boxPaidOrdersWrapperStyles = {
+  margin: { xs: '10px 16px', sm: '10px 24px' },
+};
+
+export const boxPaidOrderWrapperTitlesStyles = {
+  display: 'flex',
+  flexDirection: { xs: 'column', sm: 'row' },
+  width: '100%',
+};
+
+export const orderPaidIdTitleStyles = {
+  width: { xs: '100%', sm: '50%' },
+  fontSize: { xs: '1.2rem', sm: '1.2rem', mxd: '1.4rem' },
+  textTransform: 'uppercase',
+  fontFamily: 'Oswald, sans-serif',
+  fontWeight: 300,
+};
+
+export const dateOrderPaidTitleStyles = {
+  width: { xs: '100%', sm: '30%' },
+  color: 'text.secondary',
+  fontSize: { xs: '1.2rem', sm: '1rem', mxd: '1.2rem' },
+};
+
+export const deliveredOrderPaidTitleStyles = {
+  display: 'flex',
+  justifyContent: { xs: 'initial', sm: 'center' },
+  alignItems: 'center',
+  width: { xs: '100%', sm: '20%' },
+  color: 'text.secondary',
+  fontSize: { xs: '1.2rem', sm: '1rem', mxd: '1.2rem' },
+  '& .MuiSvgIcon-root': {
+    fontSize: { xs: '1.2rem', sm: '1rem', mxd: '1.2rem' },
+    transform: 'initial',
+  },
+};
+
+export const iconTickOrdersPaidStyles = {
+  marginLeft: '3px',
+  color: '#2ecc40',
+  transform: 'translateY(-1px)',
+};
+export const iconNoOrdersPaidStyles = {
+  marginLeft: '3px',
+  color: '#ff4136',
+  transform: 'translateY(-1px)',
+};
+
+export const titleShippingOrdersPaidStyles = {
+  fontSize: '1.2rem',
+  textTransform: 'uppercase',
+  fontFamily: 'Oswald, sans-serif',
+  fontWeight: '400',
+  letterSpacing: '1px',
+};
+
+export const boxWrapperContentOrdersPaidStyles = {
+  display: 'flex',
+  flexDirection: { xs: 'column', sm: 'row' },
+};
+
+export const boxLeftRightOrderPaidStyles = {
+  width: { xs: '100%', sm: 'calc(50% - 7.5px)' },
+};
+
+export const cardOrderPaidStyles = {
+  display: 'flex',
+  flexDirection: { xs: 'column', sm: 'row' },
+  width: '100%',
+  padding: { xs: '10px 0px 0px 0px', sm: '5px 0px' },
+  margin: '7px 0',
+};
+
+export const mediaCardOrderPaidStyles = {
+  width: { xs: '100%', sm: '50px' },
+  height: { xs: '150px', sm: '50px' },
+  objectFit: 'contain',
+};
+
+export const cardContentWrapperOrderPaidStyles = {
+  width: { xs: '100%', sm: 'calc(100% - 50px)' },
+  display: 'flex',
+  flexDirection: { xs: 'column', sm: 'column', mxd: 'row' },
+  justifyContent: { xs: 'initial', sm: 'space-around' },
+  alignItems: { mxd: 'center' },
+  padding: 'initial',
+  '&.MuiCardContent-root:last-child': {
+    paddingBottom: 'initial',
+  },
+};
+
+export const cardContentItemNameOrderPaidStyles = {
+  width: { xs: '100%', mxd: '50%' },
+  margin: { xs: '5px 0px 0px 5px', sm: '0px', mxd: '0px 0px 0px 5px' },
+  fontSize: '1.2rem',
+  textTransform: 'uppercase',
+  fontFamily: 'Oswald, sans-serif',
+  fontWeight: '300',
+  letterSpacing: '1px',
+};
+
+export const cardContentItemAmountOrderPaidStyles = {
+  width: { xs: '100%', mxd: '25%' },
+  margin: {
+    xs: '20px 0px 0px 5px',
+    sm: 'initial',
+  },
+  fontSize: '1rem',
+};
+
+export const cardContentPriceOrderPaidStyles = {
+  width: { xs: '100%', mxd: '25%' },
+  margin: {
+    xs: '3px 0px 0px 5px',
+    sm: 'initial',
+  },
+  paddingBottom: { xs: '10px', sm: 'initial' },
+  fontSize: '1rem',
+};
+
+export const orderPaidDetailsTextStyles = {
+  fontSize: '1.2rem',
+  fontWeight: '300',
+};
+
+export const titleOrderPaidIconsStyles = {
+  display: 'flex',
+  alignItems: 'center',
+  fontSize: '1.2rem',
+  fontWeight: '300',
+  '& .MuiSvgIcon-root': {
+    fontSize: '1.2rem',
+    transform: 'initial',
+  },
+};
