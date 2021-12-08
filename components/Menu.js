528880f8ce9a673ac7fba3ce399103b94e4aabd2
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-12-08 14:50:48 +0100

    Add functionality to highlight options in menu

--- components/Menu.js
+++ components/Menu.js
@@ -1,6 +1,11 @@
-import { Fragment, useRouter } from 'next/router';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/router';
 import React, { useContext, useRef } from 'react';
-import { List, ListItem, ListItemText, Typography } from '@mui/material';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
+
 import {
   listStyles,
   listItemStyles,
@@ -13,6 +18,7 @@ import { StoreContext } from '../utils/store';
 import {
   CLOSE_DRAWER,
   OPEN_DRAWER,
+  SELECT_OPTION_MENU,
   SHOW_CONTACT,
   SHOW_SIGN_IN,
   SHOW_SIGN_UP,
@@ -20,33 +26,74 @@ import {
 
 const Menu = () => {
   const router = useRouter();
-  const { disptachContentDrawer, disptachOpenDrawer, stateLoginUser } =
-    useContext(StoreContext);
+  const {
+    disptachContentDrawer,
+    disptachOpenDrawer,
+    stateLoginUser,
+    stateOpenDrawer,
+  } = useContext(StoreContext);
   const idTimeout = useRef(null);
   const idTimeoutSec = useRef(null);
   const idTimeoutThird = useRef(null);
 
   const userId = stateLoginUser?.user?._id;
 
-  const handleItemMenu = (e) => {
+  const checkUrl = () => {
+    const isOrdersUrl = router.asPath.indexOf('orders/user?id');
+    const isProfileUrl = router.asPath.indexOf('profile/user-profile');
+
+    const isExistUrl = {
+      orderUrl: isOrdersUrl !== -1 ? 2 : false,
+      profileUrl: isProfileUrl !== -1 ? 3 : false,
+    };
+
+    const { orderUrl, profileUrl } = isExistUrl;
+
+    if (Boolean(orderUrl)) {
+      Cookies.set('_mso', '2');
+      return { orderUrl };
+    }
+    if (Boolean(profileUrl)) {
+      Cookies.set('_mso', '3');
+      return { profileUrl };
+    }
+    Cookies.remove('_mso');
+    return { orderUrl: false, profileUrl: false };
+  };
+
+  const handleItemMenu = (e, index) => {
+    disptachOpenDrawer({ type: SELECT_OPTION_MENU, selectOption: index });
+
     switch (e.target.textContent) {
+      case 'Home':
+        Cookies.remove('_mso');
+        disptachOpenDrawer({ type: CLOSE_DRAWER });
+        router.push('/');
+        break;
       case 'Contact':
         disptachOpenDrawer({ type: CLOSE_DRAWER });
-
         idTimeoutThird.current = setTimeout(() => {
           disptachOpenDrawer({ type: OPEN_DRAWER });
           disptachContentDrawer({ type: SHOW_CONTACT });
+          const { orderUrl, profileUrl } = checkUrl();
+          disptachOpenDrawer({
+            type: SELECT_OPTION_MENU,
+            selectOption:
+              profileUrl || orderUrl ? (profileUrl ? profileUrl : orderUrl) : 0,
+          });
           clearTimeout(idTimeoutThird.current);
         }, 800);
         break;
       case 'Orders':
         router.push(`/orders/user?id=${userId ? userId : 'not-found'}`);
+        Cookies.set('_mso', '2');
         disptachOpenDrawer({ type: CLOSE_DRAWER });
         break;
       case 'Profile':
         router.push(
           `/profile/user-profile?id=${userId ? userId : 'not-found'}`
         );
+        Cookies.set('_mso', '3');
         disptachOpenDrawer({ type: CLOSE_DRAWER });
         break;
       case 'Sign In':
@@ -55,6 +102,7 @@ const Menu = () => {
         idTimeout.current = setTimeout(() => {
           disptachOpenDrawer({ type: OPEN_DRAWER });
           disptachContentDrawer({ type: SHOW_SIGN_IN });
+          disptachOpenDrawer({ type: SELECT_OPTION_MENU, selectOption: 0 });
           clearTimeout(idTimeout.current);
         }, 800);
         break;
@@ -64,9 +112,12 @@ const Menu = () => {
         idTimeout.current = setTimeout(() => {
           disptachOpenDrawer({ type: OPEN_DRAWER });
           disptachContentDrawer({ type: SHOW_SIGN_UP });
+          disptachOpenDrawer({ type: SELECT_OPTION_MENU, selectOption: 0 });
           clearTimeout(idTimeoutSec.current);
         }, 800);
         break;
+      case 'Log out':
+        Cookies.remove('_mso');
       default:
         return null;
     }
@@ -88,10 +139,14 @@ const Menu = () => {
           'Sign In',
           'Sign Up',
           'Log out',
-        ].map((item) => (
-          <ListItem button key={item} onClick={handleItemMenu}>
+        ].map((item, index) => (
+          <ListItemButton
+            key={item}
+            onClick={(e) => handleItemMenu(e, index)}
+            selected={stateOpenDrawer.selectOption === index}
+          >
             <ListItemText primary={item} sx={listItemStyles} />
-          </ListItem>
+          </ListItemButton>
         ))}
       </List>
     </Nav>
