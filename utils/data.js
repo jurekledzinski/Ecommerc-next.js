d2cbd1cf17fdc78c7692c5b08ca0c9f73c92d2da
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-12-09 10:37:07 +0100

    Remove unnecessary data

---
+++ utils/data.js
@@ -0,0 +1,47 @@
+export const sliderData = [
+  {
+    id: 1,
+    image:
+      'https://ik.imagekit.io/mdklwracd5rti/Shoppy/Phones/Vivo/Vivo-s1-front_bM-RpVBvTf.png',
+    alt: 'VIVO s1 pro',
+    title: 'VIVO S1 PRO',
+    description:
+      'VIVO S1 PRO will let you multitask on the go, without worrying about ...',
+    redirect:
+      'http://localhost:3000/phones/vivo/model/vivo-s1-pro?id=618a6e500694c22e1517574a',
+  },
+  {
+    id: 2,
+    image:
+      'https://ik.imagekit.io/mdklwracd5rti/Shoppy/Phones/Apple/Iphone-12-front-back_wNvd62N3T1.png',
+    alt: 'Iphone 12',
+    title: 'IPHONE 12',
+    description:
+      'Modern, elegant and efficient. This smartphone model is one of the best choices ...',
+    redirect:
+      'http://localhost:3000/phones/apple/model/iphone-12?id=618a6e500694c22e15175736',
+  },
+  {
+    id: 3,
+    image:
+      'https://ik.imagekit.io/mdklwracd5rti/Shoppy/Phones/Oneplus/Oneplus-9-front__WxNKLz--.png',
+    alt: 'Oneplus 9',
+    title: 'ONEPLUS 9',
+    description:
+      'The ONEPLUS 9 has a 6.55-inch AMOLED panel with sloping sides, a wide colour gamut ...',
+    redirect:
+      'http://localhost:3000/phones/oneplus/model/oneplus-9?id=618a6e500694c22e15175744',
+  },
+];
+
+export const optionsInMenu = [
+  { name: 'Home', logged: true, notLogged: true },
+  { name: 'Contact', logged: true, notLogged: true },
+  { name: 'Orders', logged: true, notLogged: false },
+  { name: 'Profile', logged: true, notLogged: false },
+  { name: 'Sign In', logged: false, notLogged: true },
+  { name: 'Sign Up', logged: false, notLogged: true },
+  { name: 'Log out', logged: true, notLogged: false },
+];
+
+export const headersCart = ['Product', 'Qty', 'Price', 'Total'];
