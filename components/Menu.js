351eaa2f11b729f8a7f52bec96021d5ccc5614c6
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-12-14 15:15:00 +0100

    Select home option menu when user log out

--- components/Menu.js
+++ components/Menu.js
@@ -135,10 +135,13 @@ const Menu = () => {
 
         if (result.msgSuccess) {
           setSuccessMsg(result.msgSuccess);
-          idTimeout.current = setTimeout(
-            () => disptachOpenDrawer({ type: CLOSE_DRAWER }),
-            1100
-          );
+          idTimeout.current = setTimeout(() => {
+            disptachOpenDrawer({ type: CLOSE_DRAWER });
+            disptachOpenDrawer({
+              type: SELECT_OPTION_MENU,
+              selectOption: 0,
+            });
+          }, 1100);
         }
         Cookies.remove('darkmode');
         Cookies.remove('_mso');
