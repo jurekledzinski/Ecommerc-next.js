272f30e1288a59c25e5f4b1c7b5dfa38544ef955
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-12-16 12:47:25 +0100

    Add custom favicons

--- pages/_app.js
+++ pages/_app.js
@@ -127,6 +127,19 @@ function MyApp({ Component, pageProps }) {
                 name="description"
                 content="Buy phones, watches, tablets in good and cheap price"
               />
+              <link rel="icon" href="/favicon.ico" />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/images/favicon-16x16.png"
+              />
             </Head>
             <NextNprogress
               color="#0074d9"
